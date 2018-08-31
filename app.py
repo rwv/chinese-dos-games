6a83ef445320a23c2933b3e7a008dcd3a8d09c46
seedgou <i@zczc.cz>
2018-08-31 23:49:11 +0800

    add saves keymaps links css

--- app.py
+++ app.py
@@ -7,29 +7,48 @@ from packager import generate_script
 app = Flask(__name__)
 
 
-@app.route('/games/<gamename>/')
-def game(gamename):
-    game_info = game_infos["games"][gamename]
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+@app.route('/about')
+def about():
+    return "this is aboud"
+
+
+@app.route('/games/<identifier>/')
+def game(identifier):
+    game_info = game_infos["games"][identifier]
     return render_template('game.html',
-                           script_content=generate_script(game_info["executeFile"], game_info["script"]))
+                           script_content=generate_script(game_info["executable"], game_info["script"]),
+                           game_info=game_info)
 
 
-@app.route('/games/<gamename>/game.data')
-def game_data(gamename):
-    return redirect(url_for('static', filename='gamedata/{}/game.data'.format(gamename)), code=301)
+@app.route('/games/<identifier>/game.data')
+def game_data(identifier):
+    return redirect(url_for('static', filename='gamedata/{}/game.data'.format(identifier)), code=301)
 
 
-@app.route('/games/<gamename>/saves/<save_number>/')
-def game_save(gamename, save_number):
-    game_info = game_infos["games"][gamename]
+@app.route('/games/<identifier>/saves/')
+def game_saves(identifier):
+    game_info = game_infos["games"][identifier]
+    return render_template('saves.html', game_info=game_info)
+
+
+@app.route('/games/<identifier>/saves/<save_number>/')
+def game_save(identifier, save_number):
+    game_info = game_infos["games"][identifier]
     return render_template('game.html',
-                           script_content=generate_script(game_info["saveExecute"].format(save_number),
-                                                          game_info['script']))
+                           script_content=generate_script(game_info["executable"].format(save_number),
+                                                          game_info['script']),
+                           game_info=game_info,
+                           current_save=game_info['sampleSaves']['descriptions'][int(save_number)])
 
 
-@app.route('/games/<gamename>/saves/<save_number>/game.data')
-def game_save_data(gamename, save_number):
-    return redirect(url_for('static', filename='gamedata/{}/game.data'.format(gamename)), code=301)
+@app.route('/games/<identifier>/saves/<save_number>/game.data')
+def game_save_data(identifier, save_number):
+    return game_data(identifier)
 
 
 if __name__ == '__main__':
