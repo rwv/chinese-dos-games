db896a5e88ae00ca70e8c0c4e4442e4c5812f002
seedgou <i@zczc.cz>
2018-09-01 01:30:40 +0800

    generate check and make game.data directory

--- generate.py
+++ generate.py
@@ -13,9 +13,12 @@ def main():
         game_info = json.loads(f.read())
 
     scripts_dict = dict()
-    for k, v in game_info["games"].items():
-        scripts_dict[k] = run_packager(os.path.join(root, 'games', 'XianJianQiXiaZhuan'),
-                                       os.path.join(root, 'static', 'gamedata', k, 'game.data'))
+    for k, game in game_info["games"].items():
+        data_store_folder = os.path.join(root, 'static', 'gamedata', k)
+        if not os.path.isdir(data_store_folder):
+            os.makedirs(data_store_folder)
+        scripts_dict[k] = run_packager(os.path.join(root, 'games', game['gameFolder']),
+                                       os.path.join(data_store_folder, 'game.data'))
     with open(os.path.join(root, 'scripts.json'), mode='w', encoding='utf8') as f:
         f.write(json.dumps(scripts_dict, ensure_ascii=False))
 
