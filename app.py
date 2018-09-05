9448618887adaeb3ec6b894ba9f2270b35992579
seedgou <i@zczc.cz>
2018-09-05 16:52:56 +0800

    trival

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask import render_template, redirect, url_for
 
 from game_infos import game_infos
-from packager import generate_script
 
 app = Flask(__name__)
 
@@ -20,9 +19,7 @@ def about():
 @app.route('/games/<identifier>/')
 def game(identifier):
     game_info = game_infos["games"][identifier]
-    return render_template('game.html',
-                           script_content=generate_script(game_info["executable"], game_info["script"]),
-                           game_info=game_info)
+    return render_template('game.html', game_info=game_info)
 
 @app.route('/games/<identifier>/logo/emularity_color_small.png')
 def emularity_logo(identifier):
