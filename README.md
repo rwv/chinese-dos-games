80140d7ffa2310eb9b0b58d707d9b95e83c4b533
scy <835410808@qq.com>
2018-09-06 09:53:47 +0800

    完善下载游戏文件脚本

--- README.md
+++ README.md
@@ -6,18 +6,7 @@
 
 ### 下载游戏文件
 
-在根目录下运行以下 python 3 代码
-
-``` python
-import urllib.request, os
-from game_infos import game_infos
-
-PREFIX = "https://dos.zczc.cz/static/gamedata/"
-DESTINATION = os.path.normcase('static/gamedata/')
-
-for identifier in game_infos['games'].keys():
-    urllib.request.urlretrieve(PREFIX + identifier + '.zip', os.path.join(DESTINATION, identifier + '.zip'))
-```
+在根目录下运行`download_data.py`
 
 ### 运行网页
 
---
+++ download_data.py
@@ -0,0 +1,17 @@
+import urllib.request, os
+from game_infos import game_infos
+
+PREFIX = "https://dos.zczc.cz/static/gamedata/"
+DESTINATION = os.path.normcase('static/gamedata/')
+
+# 创建文件夹
+folder = os.path.exists(DESTINATION)
+if not folder:
+	os.makedirs(DESTINATION)
+
+opener=urllib.request.build_opener()
+opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+urllib.request.install_opener(opener)
+
+for identifier in game_infos['games'].keys():
+    urllib.request.urlretrieve(PREFIX + urllib.parse.quote(identifier) + '.zip', os.path.join(DESTINATION, identifier + '.zip'))
